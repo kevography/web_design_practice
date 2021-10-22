62815be1d19136fd580824c810a001c19eb0fca8
Kevin Chau <kevchau99@gmail.com>
2021-10-22 13:13:57 -0700

    updated wiki page with slideshow and accordion panels

--- js/main.js
+++ js/main.js
@@ -59,4 +59,22 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function() {
+   /* Toggle between adding and removing the "active" class,
+   to highlight the button that controls the panel */
+   this.classList.toggle("active");
+
+   /* Toggle between hiding and showing the active panel */
+   var panel = this.nextElementSibling;
+   if (panel.style.display === "block") {
+     panel.style.display = "none";
+   } else {
+     panel.style.display = "block";
+   }
+ });
 } 
